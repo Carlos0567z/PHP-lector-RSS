85a68d7f0e620f0da1bc28709351900677f0555a
CarlosAL <carlosalmendras66@gmail.com>
2025-12-11 14:41:19 +0100

    Update RSSElMundo.php

--- api/RSSElMundo.php
+++ api/RSSElMundo.php
@@ -1,76 +1,60 @@
 <?php
-
 require_once __DIR__ . '/conexionRSS.php';
 require_once __DIR__ . '/conexionBBDD.php';
 
-$sXML=download("https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml");
+$sXML = download("https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml");
 
-$oXML=new SimpleXMLElement($sXML);
+if (strpos($sXML, '<error>') !== false || empty($sXML)) {
+    return;
+}
 
-require_once "conexionBBDD.php";
+$oXML = new SimpleXMLElement($sXML);
 
+if ($link) {
+    $categoria = ["Política", "Deportes", "Ciencia", "España", "Economía", "Música", "Cine", "Europa", "Justicia"];
 
-if(mysqli_connect_error()){
-    printf("Conexión a el periódico El Mundo ha fallado");
-}else{
+    foreach ($oXML->channel->item as $item) {
+        $categoriaFiltro = "";
 
-    $contador=0;
-    
-    $categoria=["Política","Deportes","Ciencia","España","Economía","Música","Cine","Europa","Justicia"];
-    $categoriaFiltro="";
-
-    foreach ($oXML->channel->item as $item){ //es un for a la que le hemos dicho que extraer y donde almacenarlo
-
-       
         $media = $item->children("media", true);
-        $description = $media->description; 
-       
+        $descripcionRaw = $media->description; 
 
-        for ( $i=0 ;$i<count($item->category); $i++){ 
-
-             for($j=0; $j<count($categoria); $j++){
-
-                            if($item->category[$i]==$categoria[$j]){
-                                $categoriaFiltro="[".$categoria[$j]."]".$categoriaFiltro;
-                            }
-                        }            
-
-
+        for ($i = 0; $i < count($item->category); $i++) {
+            for ($j = 0; $j < count($categoria); $j++) {
+                if ($item->category[$i] == $categoria[$j]) {
+                    $categoriaFiltro = "[" . $categoria[$j] . "]" . $categoriaFiltro;
+                }
+            }
         }
 
+        $fPubli = strtotime($item->pubDate);
+        $new_fPubli = date('Y-m-d', $fPubli);
 
-         $fPubli= strtotime($item->pubDate);
-         $new_fPubli= date('Y-m-d', $fPubli);
+        $Repit = false;
 
-         $media = $item->children("media", true);
-         $description = $media->description; 
+        $sql = "SELECT link FROM elmundo";
+        $result = mysqli_query($link, $sql);
 
-         $sql="SELECT link FROM elmundo";
-         $result= mysqli_query($link,$sql);
-         
-         while($sqlCompara=mysqli_fetch_array($result)){
-                      
-                     
-                 if($sqlCompara['link']==$item->link){
-                     
-                    $Repit=true;
-                    $contador=$contador+1;
-                    $contadorTotal=$contador;
+        if ($result) {
+            while ($sqlCompara = mysqli_fetch_array($result)) {
+                if ($sqlCompara['link'] == $item->link) {
+                    $Repit = true;
                     break;
-                    } else {
-                        $Repit=false;
-                    }
-                    
-                   
-                    }
-                     if($Repit==false && $categoriaFiltro<>""){
-                        
-                        $sql="INSERT INTO elmundo VALUES('','$item->title','$item->link','$description','$categoriaFiltro','$new_fPubli','$item->guid')";
-                        $result= mysqli_query($link, $sql);
-                       
-                       
-                } 
-                $categoriaFiltro="";
+                }
+            }
+        }
 
+        if ($Repit == false && $categoriaFiltro != "") {
+            
+            $titulo = mysqli_real_escape_string($link, $item->title);
+            $enlace = mysqli_real_escape_string($link, $item->link);
+            $desc   = mysqli_real_escape_string($link, $descripcionRaw);
+            $cat    = mysqli_real_escape_string($link, $categoriaFiltro);
+            $guid   = mysqli_real_escape_string($link, $item->guid);
+
+            $sql = "INSERT INTO elmundo VALUES(NULL, '$titulo', '$enlace', '$desc', '$cat', '$new_fPubli', '$guid')";
+            mysqli_query($link, $sql);
+        }
     }
 }
+?>
