bdf0e4ac0ddb4891fabb2cc1ce803381ee0f6bf1
CarlosAL <carlosalmendras66@gmail.com>
2025-12-11 18:34:34 +0100

    Update RSSElMundo.php

--- api/RSSElMundo.php
+++ api/RSSElMundo.php
@@ -3,12 +3,12 @@ require_once __DIR__ . '/conexionRSS.php';
 require_once __DIR__ . '/conexionBBDD.php';
 
 $urls_elmundo = [
-    "https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/internacional.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/economia.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/cultura.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/deportes.xml" 
+    "https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml",       
+    "https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml",        
+    "https://e00-elmundo.uecdn.es/elmundo/rss/internacional.xml", 
+    "https://e00-elmundo.uecdn.es/elmundo/rss/economia.xml",      
+    "https://e00-elmundo.uecdn.es/elmundo/rss/cultura.xml",       
+    "https://e00-elmundo.uecdn.es/elmundo/rss/deportes.xml"       
 ];
 
 $misFiltros = [
@@ -43,8 +43,10 @@ foreach ($urls_elmundo as $urlFeed) {
                     foreach ($misFiltros as $filtro) {
                         if (mb_stripos($catLimpia, $filtro) !== false) {
                             $etiquetaFinal = ucfirst($filtro); 
+                            // Unificar
                             if ($etiquetaFinal == "Politica") $etiquetaFinal = "Política";
                             if ($etiquetaFinal == "Musica") $etiquetaFinal = "Música";
+                            if ($etiquetaFinal == "Economia") $etiquetaFinal = "Economía";
                             
                             if (strpos($categoriaParaGuardar, "[" . $etiquetaFinal . "]") === false) {
                                 $categoriaParaGuardar .= "[" . $etiquetaFinal . "]";
@@ -52,22 +54,23 @@ foreach ($urls_elmundo as $urlFeed) {
                         }
                     }
                 }
+
                 if ($categoriaParaGuardar == "") {
                     if (strpos($urlFeed, 'deportes') !== false) $categoriaParaGuardar = "[Deportes]";
                     elseif (strpos($urlFeed, 'economia') !== false) $categoriaParaGuardar = "[Economía]";
                     elseif (strpos($urlFeed, 'cultura') !== false) $categoriaParaGuardar = "[Cultura]";
                     elseif (strpos($urlFeed, 'espana') !== false) $categoriaParaGuardar = "[España]";
                     elseif (strpos($urlFeed, 'internacional') !== false) $categoriaParaGuardar = "[Internacional]";
-                    else $categoriaParaGuardar = "[General]";
+                    else $categoriaParaGuardar = "[General]"; 
                 }
 
-                
                 $enlace = mysqli_real_escape_string($link, $item->link);
 
                 $checkSQL = "SELECT link FROM elmundo WHERE link = '$enlace' LIMIT 1";
                 $checkResult = mysqli_query($link, $checkSQL);
 
                 if (mysqli_num_rows($checkResult) == 0) {
+                    
                     $media = $item->children("media", true);
                     if (isset($media->description) && !empty($media->description)) {
                         $descripcionRaw = (string)$media->description;
@@ -81,17 +84,16 @@ foreach ($urls_elmundo as $urlFeed) {
                     $titulo = mysqli_real_escape_string($link, $item->title);
                     $desc   = mysqli_real_escape_string($link, $descripcionRaw);
                     $cat    = mysqli_real_escape_string($link, $categoriaParaGuardar);
-                    $guid   = mysqli_real_escape_string($link, $item->guid);
-
-                    $sql = "INSERT INTO elmundo (cod, titulo, link, descripcion, categoria, fPubli, guid) 
-                            VALUES (NULL, '$titulo', '$enlace', '$desc', '$cat', '$new_fPubli', '$guid')";
                     
-                    if(mysqli_query($link, $sql)){
-                    }
+                    $contenido = mysqli_real_escape_string($link, $item->guid);
+
+                    $sql = "INSERT INTO elmundo (cod, titulo, link, descripcion, categoria, fPubli, contenido) 
+                            VALUES (NULL, '$titulo', '$enlace', '$desc', '$cat', '$new_fPubli', '$contenido')";
+                    
+                    mysqli_query($link, $sql);
                 }
             }
         }
     }
 }
-echo "Proceso de El Mundo finalizado.";
 ?>
